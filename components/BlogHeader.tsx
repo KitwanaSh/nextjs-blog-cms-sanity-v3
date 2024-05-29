b4c7a8e3375b9b973298b534c10a493e66f90995
Kitwana Ezechiel <rodriguekitweze@gmail.com>
2024-05-29 12:58:40 +0000

    Feat: Update the console

--- components/BlogHeader.tsx
+++ components/BlogHeader.tsx
@@ -15,16 +15,18 @@ export default function BlogHeader({
   switch (level) {
     case 1:
       return (
-        <header className="mb-10 mt-16 flex flex-col items-center md:mb-12 md:flex-row md:justify-between text-pretty">
+        <header className="mb-10 mt-10 flex flex-col items-center md:mb-12 md:flex-row md:justify-between text-pretty">
           <h1 className="text-6xl font-bold leading-tight tracking-tighter md:pr-8 md:text-8xl">
-            {title}
+            FortCongo
           </h1>
           <h4
             className={`mt-5 text-center text-lg md:pl-8 md:text-left ${styles.portableText}`}
           >
-            <PortableText value={description} />
+            {/* <PortableText value={description} /> */}
+            Rendre le Congo plus fort que jamais
           </h4>
         </header>
+
       )
 
     case 2:
